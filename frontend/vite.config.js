4e11cf6220ba47cfb188d6de7499bdca3a5e2dba
FizOkor <okorfavour2000@gmail.com>
2025-06-29 09:47:17 +0100

    config refactor

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "src/client", // frontend source directory
+  build: {
+    outDir: path.resolve(__dirname, "dist"), // build to root/dist
+    emptyOutDir: true,
+  },
   plugins: [
-    react(), 
+    react(),
     tailwindcss({
       config: "./tailwind.config.js", // Explicit path to config
-    })],
-    server: {
-      historyApiFallback: true,
+    }),
+  ],
+  server: {
+    historyApiFallback: true,
   },
 });
