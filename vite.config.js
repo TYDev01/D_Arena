3d5f8257f07609198b8ea64dafcc8fc241046783
FizOkor <okorfavour2000@gmail.com>
2025-06-29 11:41:57 +0100

    reconfigure 2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: "client", // frontend source directory
+  // root: "client", // frontend source directory
+  base: "/",
   build: {
     outDir: "dist",
     emptyOutDir: true,
