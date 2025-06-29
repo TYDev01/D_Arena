901304ad0887f439403139a139e87a8fe74281bb
FizOkor <okorfavour2000@gmail.com>
2025-06-29 12:33:00 +0100

    reconfigure 6

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from 'path';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // root: "client", // frontend source directory
-  base: "/",
+  root: "client",
   build: {
-    outDir: "./dist",
+    outDir: "../dist",
     emptyOutDir: true,
   },
   plugins: [
